33a66848de396987753ad26796b66c32c7924b2a
alex dionicio <alexdionicio934@hotmail.com>
2021-01-22 01:01:40 -0400

    password reset and email functionality

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -123,4 +123,11 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = "login"
 LOGOUT_URL = "logout"
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER= 'add_your_email'
+EMAIL_HOST_PASSWORD= 'add_your_pass'
+EMAIL_PORT= 587
+EMAIL_USE_TLS= True
  
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
