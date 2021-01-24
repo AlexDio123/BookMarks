332d6d2c769860769d4c3f5bca7fb1ca5d4df117
alex dionicio <alexdionicio934@hotmail.com>
2021-01-23 23:49:05 -0400

    Authentication with facebook

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -142,5 +142,15 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
     'social_core.backends.facebook.FacebookOAuth2',
-    
+
 ]
+
+SOCIAL_AUTH_FACEBOOK_KEY = '######' #Facebook App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = '########' #Facebook app Secret
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
+SOCIAL_AUTH_TWITTER_KEY = '' # Twitter API Key
+SOCIAL_AUTH_TWITTER_SECRET = '' # Twitter API Secret
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '' # Google Consumer Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '' # Google Consumer Secret
